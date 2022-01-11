ecc197540736065bfb4a59e6319667464b39616d
Romain <piccolo.rom@gmail.com>
2022-01-11 15:56:12 +0100

    issue1 close modal done

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// launch close event
+modalCloseBtn.addEventListener("click", closeModal);
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+// close modal
+function closeModal() {
+    modalbg.style.display = "none";
 }
 
 
